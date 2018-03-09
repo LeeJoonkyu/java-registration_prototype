85310483d1fcf9fe1d93649d2df063798c9471e1
Joonkyu <ljk6463@gmail.com>
2018-03-09 23:02:10 +0900

    배열을 모두 LinkedList 인터페이스로 교체, 선수과목 체크 프로그래밍중

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ package test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 class LastYear{
@@ -45,9 +46,8 @@ public class Test {
 		File file = new File("origin12.txt");
 		File file2 = new File("newsubject.txt");
 		Scanner scin;
-		LastYear L[]=new LastYear[13];
-		ThisYear TY[]=new ThisYear[19];
-		int i=0;
+		LinkedList<LastYear> LY = new LinkedList<LastYear>();
+		LinkedList<ThisYear> TY = new LinkedList<ThisYear>();
 
 		if(file.exists()) {
 			try {
@@ -55,7 +55,7 @@ public class Test {
 				while(scin.hasNext()) {
 					String code=scin.next();
 					String subject=scin.next();
-					L[i++]=new LastYear(code,subject);
+					LY.add(new LastYear(code,subject));
 				}
 				scin.close();
 			}
@@ -65,7 +65,6 @@ public class Test {
 			System.out.println("orgin12.txt not exist!!");
 		}
 
-		i=0;
 		if(file2.exists()) {
 			try {
 				scin=new Scanner(file2);
@@ -75,7 +74,7 @@ public class Test {
 					String time=scin.next(); //나중에는 time1 time2로 해서 중복제거해야함.
 					String professor=scin.next();
 					String pre_code=scin.next(); //선수과목이 없는경우 파일내에서 NULL코드 지정안하면 NoSuchElementException 발생.
-					TY[i++]=new ThisYear(code,subject,time,professor,pre_code);
+					TY.add(new ThisYear(code,subject,time,professor,pre_code));
 				}
 				scin.close();
 			}
@@ -94,14 +93,29 @@ public class Test {
 		System.out.println("수강 과목 코드를 입력하세요 : ");
 		//제네릭스 어래이배열 써보기. 동적이니까 만약 3과목 아래면 에러, 8과목 위여도 에러. 하계상계가 있는 배열?
 
-		ArrayList <String> SubjectArr = new ArrayList<String>(3);
-		for (i=0;i<7;++i) {
+		LinkedList<ThisYear> SubjectList = new LinkedList<ThisYear>();
+		boolean pass = true;
+
+		//선수과목 체크
+		//완료 후 강의시간 체크.
+		for (int i=0;i<7;++i) {
 			String Subject = scin2.next();
-			SubjectArr.add(Subject);//이렇게 할 경우 추가로 과목을 선택하지 않을때는 x 라던가 0이라던가 널값을 입력해줘야함.
-			for(int j=0;j<11;++j) {
-				if (f)
+			SubjectList.add(new ThisYear(Subject,"0","0","0","0"));
+			//이렇게 할 경우 추가로 과목을 선택하지 않을때는 x 라던가 0이라던가 널값을 입력해줘야함.
+		}
+
+		for(int i=0;i<7;++i) {//신청과목
+			for(int j=0;j<11;++j) {//지난해과목들
+				ListIterator<LastYear> iter = LY.listIterator();
+				if (SubjectList.get(i).pre_code=="#NULL") continue;
+				else if (SubjectList.get(i).pre_code==LY.) //신청과목에서 선수과목이, 이전해과목에 있는경우와 없는경우. 있으면 컨티뉴 없으면 페일.
+
+				}
 			}
 		}
+
+
+
 		for (String x : SubjectArr) System.out.println(x);
 	}
 }
